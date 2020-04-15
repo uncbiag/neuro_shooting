943f73eb2d99973338f8b7c7257ec94f507bdd15
François-Xavier VIALARD <vialard@ceremade.dauphine.fr>
2020-04-15 12:02:32 +0200

    Implements a composition of function approach for computing the gradients. Also modified the sign of theta.

--- demos/scalar_autograd_gradient_test_possible_fix.py
+++ demos/scalar_autograd_gradient_test_possible_fix.py
@@ -28,6 +28,27 @@ def compute_lagrangian(q,p,theta):
 
     return L, kinetic_energy, potential_energy
 
+## Here I compute the reduced lagrangian in terms of q and p
+## thus it is the composition of the lagrangian in terms of the parameters and a function
+## which is here compute_parameter_autograd
+def compute_reduced_lagrangian(q,p):
+    theta = compute_parameter_autograd(q,p)
+    kinetic_energy = 0.5 * theta * theta
+    potential_energy = p * theta * nl(q)
+    L = kinetic_energy - potential_energy
+    return L, kinetic_energy, potential_energy
+
+### hereafter, diffentiation of the reduced lagrangian wrt q
+def rhs_costate_autograd_reduced(q,p):
+    lagrangian, kinetic_energy, potential_energy = compute_reduced_lagrangian(q=q, p=p)
+    rhs_costate = autograd.grad(lagrangian, (q),
+                                      grad_outputs=lagrangian.data.new(lagrangian.shape).fill_(1),
+                                      create_graph=True,
+                                      retain_graph=True,
+                                      allow_unused=True)
+    return rhs_costate[0]
+
+
 def rhs_state(q,theta):
     rhs = theta*nl(q)
     return rhs
@@ -37,10 +58,10 @@ def rhs_costate(q,p,theta):
     return rhs
 
 def compute_parameter(q,p):
-    theta = -p*nl(q)
+    theta = p*nl(q)
     return theta
 
-def rhs_shooting_analytic(q,p):
+def rhs_shooting_analytic(q,p,if_reduced = True):
     theta = compute_parameter(q=q,p=p)
     rhs = dict()
     rhs['q'] = rhs_state(q=q,theta=theta)
@@ -85,10 +106,10 @@ def compute_parameter_autograd(q,p):
                                          retain_graph=True,
                                          allow_unused=True)
 
-    theta = -theta_grad_potential[0]
+    theta = theta_grad_potential[0]
     return theta
 
-def rhs_shooting_autograd(q,p):
+def rhs_shooting_autograd(q,p,if_reduced):
 
     theta = compute_parameter_autograd(q=q,p=p)
     # don't want to carry around any additional depdendencies when computing the costate via autograd
@@ -98,14 +119,17 @@ def rhs_shooting_autograd(q,p):
 
     rhs = dict()
     rhs['q'] = rhs_state(q=q, theta=theta)
-    rhs['p'] = rhs_costate_autograd(q=q, p=p, theta=theta)
+    if if_reduced:
+        rhs['p'] = rhs_costate_autograd_reduced(q=q, p=p)
+    else:
+        rhs["p"] = rhs_costate_autograd(q=q,p=p,theta =theta)
     return rhs
 
-def euler_forward(rhs_fcn,q0,p0,dt,nr_of_time_steps):
+def euler_forward(rhs_fcn,q0,p0,dt,nr_of_time_steps,if_reduced = True):
     q=q0
     p=p0
     for n in range(nr_of_time_steps):
-        rhs = rhs_fcn(q=q,p=p)
+        rhs = rhs_fcn(q=q,p=p,if_reduced=if_reduced)
         q = q + dt*rhs['q']
         p = p + dt*rhs['p']
     return q,p
@@ -114,10 +138,14 @@ def euler_forward(rhs_fcn,q0,p0,dt,nr_of_time_steps):
 q0 = torch.randn([1], requires_grad=True)
 p0 = torch.randn([1], requires_grad=True)
 
+# if_reduced == True then use the reduced lagrangian computation
+# if_reduced == False then use Marc's trick
+if_reduced = True
+
 # first try to compute the rhs analytically and via autograd
 rhs_analytic = rhs_shooting_analytic(q=q0, p=p0)
-rhs_autograd = rhs_shooting_autograd(q=q0, p=p0)
-
+rhs_autograd = rhs_shooting_autograd(q=q0, p=p0,if_reduced=if_reduced)
+print("Is reduced ? "+str(if_reduced))
 print('analytic: rhs_q={}, rhs_p={}'.format(rhs_analytic['q'], rhs_analytic['p']))
 print('autograd: rhs_q={}, rhs_p={}'.format(rhs_autograd['q'], rhs_autograd['p']))
 print('analytic/autograd: rhs_q={}, rhs_p={}\n'.format(rhs_analytic['q']/rhs_autograd['q'], rhs_analytic['p']/rhs_autograd['p']))
@@ -125,7 +153,7 @@ print('analytic/autograd: rhs_q={}, rhs_p={}\n'.format(rhs_analytic['q']/rhs_aut
 # now do to some simple euler-forward integration
 
 # do it for the analytic version first
-res_analytic_q, res_analytic_p = euler_forward(rhs_fcn=rhs_shooting_analytic, q0=q0,p0=p0,dt=0.1,nr_of_time_steps=2)
+res_analytic_q, res_analytic_p = euler_forward(rhs_fcn=rhs_shooting_analytic, q0=q0,p0=p0,dt=0.1,nr_of_time_steps=2,if_reduced = if_reduced)
 loss_analytic = res_analytic_q**2 + res_analytic_p**2
 loss_analytic.backward()
 
@@ -140,7 +168,7 @@ print('loss_analytic_grad_p0={}'.format(loss_analytic_grad_p0))
 zero_grad(q0)
 zero_grad(p0)
 
-res_autograd_q, res_autograd_p = euler_forward(rhs_fcn=rhs_shooting_autograd, q0=q0,p0=p0,dt=0.1,nr_of_time_steps=2)
+res_autograd_q, res_autograd_p = euler_forward(rhs_fcn=rhs_shooting_autograd, q0=q0,p0=p0,dt=0.1,nr_of_time_steps=2,if_reduced = if_reduced)
 loss_autograd = res_autograd_q**2 + res_autograd_p**2
 loss_autograd.backward()
 
