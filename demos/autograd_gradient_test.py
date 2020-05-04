Some more gradient debugging. Also fixed weighting issue in analytic solutions.

demos/autograd_gradient_test.py
@@ -1,11 +1,11 @@
 # the goal of this script is to use a simple affine model and check if the gradients are computed correctly
 
-import torch
-import random
 
+import random
 seed = 1234
 print('Setting the random seed to {:}'.format(seed))
 random.seed(seed)
+import torch
 torch.manual_seed(seed)
 
 import neuro_shooting.shooting_blocks as shooting_blocks
@@ -23,18 +23,25 @@ def zero_grads(pars):
 
 # particle setup
 nonlinearity = 'tanh'
-nr_of_particles = 5
-parameter_weight = 1.0
+nr_of_particles = 10
+parameter_weight = 0.1
 
 # create a simple integrator
 stepsize = 0.1
 integrator_options = {'step_size': stepsize}
-in_features_size = 2
+in_features_size = 1
 
 check_models = ['updown']
 #check_models = ['DEBUG']
 #check_models = ["simple"]
 #check_models = ["universal"]
+
+#check_models = ['updown','DEBUG','simple','universal']
+
+number_of_tests_passed = 0
+number_of_tests_attempted = 0
+tolerance = 5e-3
+
 integrator = generic_integrator.GenericIntegrator(integrator_library = 'odeint', integrator_name = 'rk4',
                                                           use_adjoint_integration=False,
                                                           integrator_options=integrator_options)
@@ -57,9 +64,18 @@ for current_model in check_models:
                                                                           parameter_weight=parameter_weight)
 
     elif current_model=='updown':
-        shooting_model = shooting_models.AutoShootingIntegrandModelUpDown(in_features=in_features_size, nonlinearity=nonlinearity,
-                                                                          nr_of_particles=nr_of_particles,particle_dimension = 1,particle_size = in_features_size,
-                                                                          parameter_weight=parameter_weight)
+        # shooting_model = shooting_models.AutoShootingIntegrandModelUpDown(in_features=in_features_size, nonlinearity=nonlinearity,
+        #                                                                   nr_of_particles=nr_of_particles,particle_dimension = 1,
+        #                                                                   particle_size = in_features_size,
+        #                                                                   parameter_weight=parameter_weight,
+        #                                                                   inflation_factor=5)
+
+        print('Hello, I am here!')
+        shooting_model = shooting_models.AutoShootingIntegrandModelUpDown(in_features=1, nonlinearity='relu', nr_of_particles=10,
+                                                          use_analytic_solution=True,
+                                                          particle_dimension=1, particle_size=1, parameter_weight=0.1,
+                                                          inflation_factor=5)
+
     elif current_model == 'DEBUG':
         shooting_model = shooting_models.DEBUGAutoShootingIntegrandModelSimple(in_features=in_features_size,
                                                                           nonlinearity=nonlinearity,
@@ -70,22 +86,23 @@ for current_model in check_models:
     else:
         raise ValueError('Unknown model to check: {}'.format( current_model ))
 
-    shooting_block = shooting_blocks.ShootingBlockBase(name='updown', shooting_integrand=shooting_model)
+    shooting_block = shooting_blocks.ShootingBlockBase(name='test', shooting_integrand=shooting_model)
 
     print('\n\nChecking model: {}'.format(current_model))
     print('-------------------------------------\n')
 
     # create some sample data
-    sample_data = torch.randn([20,1,in_features_size])
-
-    # run through the shooting block once to get the necessary parameters
-    shooting_block(x=sample_data)
+    sample_data = torch.randn([100,1,in_features_size])
+    sample_data_init = torch.randn([100,1,in_features_size])
 
     autodiff_gradient_results = dict()
     analytic_gradient_results = dict()
 
     for use_analytic_solution in (True,False):
         shooting_model.use_analytic_solution = use_analytic_solution
+
+        # run through the shooting block once to get the necessary parameters
+        shooting_block(x=sample_data_init)
 
         # zero gradients
         zero_grads(shooting_block.parameters())
@@ -94,7 +111,7 @@ for current_model in check_models:
         pred,_,_,_ = shooting_block(x=sample_data)
 
         # create a loss
-        loss = torch.mean(pred) + shooting_block.get_norm_penalty()
+        loss = torch.mean(pred**2) + shooting_block.get_norm_penalty()
 
         # compute gradient
         loss.backward()
@@ -107,9 +124,9 @@ for current_model in check_models:
             print('Gradient: {}'.format(par_value.grad))
 
             if use_analytic_solution:
-                analytic_gradient_results[par_name] = par_value.grad.clone().detach()
+                analytic_gradient_results[par_name] = par_value.grad.detach().clone()
             else:
-                autodiff_gradient_results[par_name] = par_value.grad.clone().detach()
+                autodiff_gradient_results[par_name] = par_value.grad.detach().clone()
 
     # now compare them
     for par_name in autodiff_gradient_results:
@@ -118,4 +135,30 @@ for current_model in check_models:
         print('Gradient ratio autodiff/analytic for parameter {}:'.format(par_name))
         print('{}'.format(autodiff_gradient/analytic_gradient))
 
-    print('Hello world')
+        number_of_tests_attempted +=1
+
+        nz_autodiff = autodiff_gradient != 0
+        nz_analytic = analytic_gradient != 0
+
+        if torch.all(nz_analytic == nz_autodiff).item():
+            rel_error = torch.max(torch.abs(autodiff_gradient[nz_autodiff] / analytic_gradient[nz_analytic] - 1))
+            if rel_error < tolerance:
+                number_of_tests_passed += 1
+                print('\nPASSED with relative error of {}\n'.format(rel_error))
+            else:
+                print('\n-------------------------------------')
+                print('FAILED with relative error of {}'.format(rel_error))
+                print('-------------------------------------\n')
+        else:
+            print('\n-------------------------------------')
+            print('FAILED due to different ZERO pattern')
+            print('-------------------------------------\n')
+
+print('\nOverall summary:')
+print('-----------------\n')
+print('Passed {}/{} tests.'.format(number_of_tests_passed,number_of_tests_attempted))
+print('Failed {}/{} tests.'.format(number_of_tests_attempted-number_of_tests_passed,number_of_tests_attempted))
+
+if number_of_tests_passed==number_of_tests_attempted:
+    print('\nCongratulations, all tests passes at a tolerance level of {}'.format(tolerance))
+

demos/simple_functional_mapping_example.py
@@ -11,12 +11,16 @@ import numpy as np
 from collections import OrderedDict
 from collections import defaultdict
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 import torch.optim as optim
 from torchdiffeq import odeint
+
+import neuro_shooting
+import neuro_shooting.shooting_blocks as sblocks
+import neuro_shooting.shooting_models as smodels
+import neuro_shooting.generic_integrator as gi
+import neuro_shooting.parameter_initialization as pi
+
+import simple_discrete_neural_networks as sdnn
 
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
@@ -24,14 +28,8 @@ import matplotlib.pyplot as plt
 import os
 
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
-
 sys.path.insert(0, '../neuro_shooting')
 
-import neuro_shooting
-import neuro_shooting.shooting_blocks as sblocks
-import neuro_shooting.shooting_models as smodels
-import neuro_shooting.generic_integrator as gi
-import neuro_shooting.parameter_initialization as pi
 
 def setup_cmdline_parsing():
     parser = argparse.ArgumentParser('Shooting spiral')
@@ -51,6 +49,7 @@ def setup_cmdline_parsing():
     parser.add_argument('--inflation_factor', type=int, default=5, help='Multiplier for state dimension for updown shooting model types')
     parser.add_argument('--use_particle_rnn_mode', action='store_true', help='When set then parameters are only computed at the initial time and used for the entire evolution; mimicks a particle-based RNN model.')
     parser.add_argument('--use_particle_free_rnn_mode', action='store_true', help='This is directly optimizing over the parameters -- no particles here; a la Neural ODE')
+    parser.add_argument('--use_parameter_penalty_energy', action='store_true', default=False)
 
     # non-shooting networks implemented
     parser.add_argument('--nr_of_layers', type=int, default=30, help='Number of layers for the non-shooting networks')
@@ -83,142 +82,6 @@ def get_uniform_sample_batch(nr_of_samples=10):
     sample_batch_out = sample_batch_in**3 # and takes them to the power of three for the output
 
     return sample_batch_in, sample_batch_out
-
-
-def replicate_modules(module,nr_of_layers, **kwargs):
-    modules = OrderedDict()
-    for i in range(nr_of_layers):
-        modules['l{}'.format(i)] = module(**kwargs)
-
-    return modules
-
-class SimpleResNetBlock(nn.Module):
-
-    def __init__(self):
-        super(SimpleResNetBlock, self).__init__()
-
-        self.l1 = nn.Linear(1,1,bias=True)
-
-    def forward(self, x):
-        x = x + self.l1(F.relu(x))
-
-        return x
-
-class UpDownResNetBlock(nn.Module):
-
-    def __init__(self, inflation_factor=5):
-        super(UpDownResNetBlock, self).__init__()
-
-        self.l1 = nn.Linear(1,inflation_factor,bias=True)
-        self.l2 = nn.Linear(inflation_factor,1,bias=True)
-
-    def forward(self, x):
-        y = self.l1(F.relu(x))
-        z = self.l2(F.relu(y))
-
-        return x + z
-
-class UpDownDoubleResNetBlock(nn.Module):
-
-    def __init__(self, inflation_factor=5):
-        super(UpDownDoubleResNetBlock, self).__init__()
-
-        self.l1 = nn.Linear(inflation_factor,1,bias=True)
-        self.l2 = nn.Linear(1,inflation_factor,bias=False)
-
-    def forward(self, x1x2):
-        x1 = x1x2[0]
-        x2 = x1x2[1]
-
-        x1 = x1 + self.l1(F.relu(x2))
-        x2 = x2 + self.l2(F.relu(x1)) # this is what an integrator would typically do
-        #x2 = self.l2(F.relu(x1))
-
-        return x1, x2
-
-class DoubleResNetUpDown(nn.Module):
-
-    def __init__(self, nr_of_layers=30, inflation_factor=5):
-        super(DoubleResNetUpDown, self).__init__()
-        print("nr_of_layers ",nr_of_layers)
-        modules = replicate_modules(module=UpDownDoubleResNetBlock,nr_of_layers=nr_of_layers, inflation_factor=inflation_factor)
-        self.model = nn.Sequential(modules)
-
-    def forward(self, x1x2):
-        return self.model(x1x2)
-
-class ResNetUpDown(nn.Module):
-
-    def __init__(self, nr_of_layers=10, inflation_factor=5):
-        super(ResNetUpDown, self).__init__()
-        modules = replicate_modules(module=UpDownResNetBlock, nr_of_layers=nr_of_layers, inflation_factor=inflation_factor)
-        self.model = nn.Sequential(modules)
-
-    def forward(self, x):
-        return self.model(x)
-
-class ResNet(nn.Module): # corresponds to our simple shooting model
-
-    def __init__(self, nr_of_layers=10):
-        super(ResNet, self).__init__()
-
-        modules = replicate_modules(module=SimpleResNetBlock, nr_of_layers=nr_of_layers)
-        self.model = nn.Sequential(modules)
-
-    def forward(self, x):
-        return self.model(x)
-
-class DoubleResNetUpDownRNN(nn.Module): # corresponds to our simple shooting model
-
-    def __init__(self, nr_of_layers=10, inflation_factor=5):
-        super(DoubleResNetUpDownRNN, self).__init__()
-
-        self.nr_of_layers = nr_of_layers
-        print("use "+str(self) + " with " + str(self.nr_of_layers) + " nr of layers")
-        self.l1 = UpDownDoubleResNetBlock(inflation_factor=inflation_factor)
-
-    def forward(self, x1x2):
-        x1 = x1x2[0]
-        x2 = x1x2[1]
-
-        for i in range(self.nr_of_layers):
-            x1, x2 = self.l1((x1, x2))
-
-        return x1, x2
-
-class ResNetRNN(nn.Module):
-
-    def __init__(self, nr_of_layers=10, inflation_factor=5):
-        super(ResNetRNN, self).__init__()
-        self.nr_of_layers = nr_of_layers
-        print("use "+ str(self) + " nr of layers " + str(self.nr_of_layers))
-
-        self.l1 = nn.Linear(inflation_factor,1,bias=True)
-        self.l2 = nn.Linear(1,inflation_factor,bias=True)
-
-    def forward(self, x):
-        for i in range(self.nr_of_layers):
-            x = x + 1./self.nr_of_layers * self.l1(F.relu(self.l2(x)))
-
-        return x
-
-class ODESimpleFunc(nn.Module):
-
-    def __init__(self):
-        super(ODESimpleFunc, self).__init__()
-
-        self.net = nn.Sequential(
-            nn.Linear(1, 1),
-            nn.Tanh(),
-        )
-
-        for m in self.net.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, mean=0, std=0.1)
-                nn.init.constant_(m.bias, val=0)
-
-    def forward(self, t, y):
-        return self.net(y)
 
 def print_all_parameters(model):
 
@@ -423,7 +286,10 @@ if __name__ == '__main__':
 
     seed = args.seed
     print('Setting the random seed to {:}'.format(seed))
+
+    import torch
     random.seed(seed)
+    np.random.seed(seed)
     torch.manual_seed(seed)
 
     device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
@@ -489,8 +355,13 @@ if __name__ == '__main__':
     use_particle_rnn_mode = args.use_particle_rnn_mode
     use_particle_free_rnn_mode = args.use_particle_free_rnn_mode
 
-    use_analytic_solution = False # just set to one value
-    write_out_first_five_gradients = True
+
+    use_analytic_solution = True # True is the proper setting here for models that have analytic solutions implemented
+    write_out_first_five_gradients = False # for debugging purposes; use jointly with check_gradient_over_iterations.py
+    use_fixed_sample_batch = write_out_first_five_gradients # has to be set to True if we want to compare autodiff and analytic gradients (as otherwise there will be different random initializations
+
+    if write_out_first_five_gradients and not use_fixed_sample_batch:
+        print('WARNING: if you want to compare autodiff/analytic gradient then use_fixed_sample_batch should be set to True')
 
     if args.shooting_model == 'simple':
         smodel = smodels.AutoShootingIntegrandModelSimple(**shootingintegrand_kwargs,use_analytic_solution=use_analytic_solution, use_rnn_mode=use_particle_rnn_mode)
@@ -507,7 +378,6 @@ if __name__ == '__main__':
         name=block_name,
         shooting_integrand=smodel,
         integrator_name=args.method,
-        use_adjoint_integration=args.adjoint,
         use_particle_free_rnn_mode=use_particle_free_rnn_mode,
         integrator_options = {'step_size': args.stepsize}
     )
@@ -519,31 +389,31 @@ if __name__ == '__main__':
         weight_decay = 0.0001
         lr = 1e-3
         print('Using ResNetRNN: weight = {}'.format(weight_decay))
-        simple_resnet = ResNetRNN(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
+        simple_resnet = sdnn.ResNetRNN(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
     elif args.use_double_resnet_rnn:
         weight_decay = 0.025
         lr = 1e-3
         print('Using DoubleResNetRNN: weight = {}'.format(weight_decay))
-        simple_resnet = DoubleResNetUpDownRNN(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
+        simple_resnet = sdnn.DoubleResNetUpDownRNN(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
     elif args.use_updown:
         weight_decay = 0.01
         lr = 1e-3
         print('Using ResNetUpDown: weight = {}'.format(weight_decay))
-        simple_resnet = ResNetUpDown(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
+        simple_resnet = sdnn.ResNetUpDown(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
     elif args.use_simple_resnet:
         weight_decay = 0.0001
         lr = 1e-2
         print('Using ResNet: weight = {}'.format(weight_decay))
-        simple_resnet = ResNet(nr_of_layers=args.nr_of_layers)
+        simple_resnet = sdnn.ResNet(nr_of_layers=args.nr_of_layers)
     elif args.use_double_resnet:
         #weight_decay = 0.025
         weight_decay = 0.01
         lr = 1e-2
         print('Using DoubleResNetUpDown: weight = {}'.format(weight_decay))
-        simple_resnet = DoubleResNetUpDown(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
+        simple_resnet = sdnn.DoubleResNetUpDown(nr_of_layers=args.nr_of_layers, inflation_factor=inflation_factor)
     elif args.use_neural_ode:
         print('Using neural ode')
-        func = ODESimpleFunc()
+        func = sdnn.ODESimpleFunc()
         optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
         integrator = gi.GenericIntegrator(integrator_library='odeint',
                                                           integrator_name=args.method,
@@ -563,6 +433,9 @@ if __name__ == '__main__':
     else:
 
         sample_batch_in, sample_batch_out = get_sample_batch(nr_of_samples=args.batch_size)
+        if use_fixed_sample_batch:
+            fixed_batch_in, fixed_batch_out = get_sample_batch(nr_of_samples=args.batch_size)
+
         sblock(x=sample_batch_in)
 
         # do some parameter freezing
@@ -587,7 +460,11 @@ if __name__ == '__main__':
     for itr in range(1, max_iter + 1):
 
         # get current batch data
-        batch_in, batch_out = get_sample_batch(nr_of_samples=args.batch_size)
+        if use_fixed_sample_batch:
+            batch_in = fixed_batch_in
+            batch_out = fixed_batch_out
+        else:
+            batch_in, batch_out = get_sample_batch(nr_of_samples=args.batch_size)
 
         optimizer.zero_grad()
 
@@ -612,7 +489,9 @@ if __name__ == '__main__':
             pred_y, _, _, _ = sblock(x=batch_in)
 
         if use_shooting:
-            loss = torch.mean((pred_y - batch_out)**2)  + args.pw * sblock.get_norm_penalty()
+            loss = torch.mean((pred_y - batch_out)**2)
+            if args.use_parameter_penalty_energy:
+                loss = loss + sblock.get_norm_penalty()
         else:
             loss = torch.mean((pred_y - batch_out)**2)
 

neuro_shooting/shooting_integrands.py
@@ -371,8 +371,8 @@ class ShootingIntegrandBase(nn.Module):
         lagrangian = kinetic_energy-potential_energy
 
         # TODO: this is just to explicity delete it
-        # if (not self.use_rnn_mode) and (self._externally_managed_rnn_parameters is None):
-        #     del parameter_objects
+        if (not self.use_rnn_mode) and (self._externally_managed_rnn_parameters is None):
+            del parameter_objects
 
         return self.nr_of_particles * lagrangian,self.nr_of_particles * kinetic_energy, self.nr_of_particles *potential_energy
 
@@ -756,8 +756,8 @@ class ShootingIntegrandBase(nn.Module):
 
             # we can now delete the parameters
             # # TODO: check if we need this
-            # if (not self.use_rnn_mode) and (self._externally_managed_rnn_parameters is None):
-            #     del parameter_objects
+            if (not self.use_rnn_mode) and (self._externally_managed_rnn_parameters is None):
+                del parameter_objects
 
             if self.transpose_state_when_forward:
                 # as we transposed the vectors before we need to transpose on the way back
@@ -1092,6 +1092,7 @@ class OptimalTransportNonLinearInParameter(NonlinearInParameterAutogradShootingI
             for ks in c_rhs_state_dict:
                 kinetic_energy += torch.sum(c_rhs_state_dict[ks]**2)
         #todo : introduce a penalty parameter.
+        # TODO: is multiplying woth the parameter_weight correct here? This should be associated with the parameters
         kinetic_energy = 0.5 * self.parameter_weight * kinetic_energy
         return kinetic_energy
 

neuro_shooting/shooting_models.py
@@ -345,12 +345,14 @@ class AutoShootingIntegrandModelUpDown(shooting.ShootingLinearInParameterVectorI
 
         # results need to be written in the respective parameter variables
         par_dict = p['l1'].get_parameter_dict()
-        par_dict['weight'] = l1
-        par_dict['bias'] = torch.mean(p1i,dim = 0)
+        weight_dict = p['l1'].get_parameter_weight_dict()
+        par_dict['weight'] = l1/weight_dict['weight']
+        par_dict['bias'] = torch.mean(p1i,dim = 0)/weight_dict['bias']
 
         par_dict2 = p['l2'].get_parameter_dict()
-        par_dict2['weight'] = l2
-        par_dict2['bias'] = torch.mean(p2i,dim = 0).t()
+        weight_dict2 = p['l2'].get_parameter_weight_dict()
+        par_dict2['weight'] = l2/weight_dict2['weight']
+        par_dict2['bias'] = (torch.mean(p2i,dim = 0).t())/weight_dict2['bias']
 
         return p
 
@@ -505,14 +507,18 @@ class AutoShootingIntegrandModelUniversal(shooting.ShootingLinearInParameterVect
         l3  = torch.mean(temp,dim = 0)
         # results need to be written in the respective parameter variables
         par_dict = p['l1'].get_parameter_dict()
-        par_dict['weight'] = l1
-        par_dict['bias'] = torch.mean(p1i,dim = 0)
+        weight_dict = p['l1'].get_parameter_weight_dict()
+        par_dict['weight'] = l1/weight_dict['weight']
+        par_dict['bias'] = torch.mean(p1i,dim = 0)/weight_dict['bias']
 
         par_dict2 = p['l2'].get_parameter_dict()
-        par_dict2['weight'] = l2
-        par_dict2['bias'] = torch.mean(p2i,dim = 0).t()
-        par_dict3 = p["l3"].get_parameter_dict()
-        par_dict3["weight"] = l3
+        weight_dict2 = p['l2'].get_parameter_weight_dict()
+        par_dict2['weight'] = l2/weight_dict2['weight']
+        par_dict2['bias'] = (torch.mean(p2i,dim = 0).t())/weight_dict2['bias']
+
+        par_dict3 = p['l3'].get_parameter_dict()
+        weight_dict3 = p['l3'].get_parameter_weight_dict()
+        par_dict3['weight'] = l3/weight_dict3['weight']
         return p
 
 class DEBUGAutoShootingIntegrandModelSimple(shooting.ShootingLinearInParameterVectorIntegrand):
@@ -676,8 +682,9 @@ class DEBUGAutoShootingIntegrandModelSimple(shooting.ShootingLinearInParameterVe
 
         # results need to be written in the respective parameter variables
         par_dict = p['l1'].get_parameter_dict()
-        par_dict['weight'] = At.t()
-        par_dict['bias'] = bt
+        weight_dict = p['l1'].get_parameter_weight_dict()
+        par_dict['weight'] = (At.t())/weight_dict['weight']
+        par_dict['bias'] = (bt)/weight_dict['bias']
 
         # ------------------
 
@@ -694,8 +701,9 @@ class DEBUGAutoShootingIntegrandModelSimple(shooting.ShootingLinearInParameterVe
 
         # results need to be written in the respective parameter variables
         par_dict = p['l2'].get_parameter_dict()
-        par_dict['weight'] = At.t()
-        par_dict['bias'] = bt
+        weight_dict = p['l2'].get_parameter_weight_dict()
+        par_dict['weight'] = (At.t())/weight_dict['weight']
+        par_dict['bias'] = (bt)/weight_dict['bias']
 
         return p
 
@@ -825,6 +833,7 @@ class AutoShootingIntegrandModelSimple(shooting.ShootingLinearInParameterVectorI
         pi = c['p_q1']
 
         par_dict = p['l1'].get_parameter_dict()
+        weight_dict = p['l1'].get_parameter_weight_dict()
         device = par_dict['weight'].device
         dtype = par_dict['weight'].dtype
 
@@ -836,8 +845,8 @@ class AutoShootingIntegrandModelSimple(shooting.ShootingLinearInParameterVectorI
         bt = 1 / self.nr_of_particles * pi.sum(dim=0)  # -\sum_i q_i
 
         # results need to be written in the respective parameter variables
-        par_dict['weight'] = At.t()
-        par_dict['bias'] = bt
+        par_dict['weight'] = (At.t())/weight_dict['weight']
+        par_dict['bias'] = (bt)/weight_dict['bias']
 
         return p
 